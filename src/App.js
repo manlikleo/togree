f66f5cf56895d6174fbd42341208c5373500b67b
manlikleo <olaotan104@gmail.com>
2020-12-04 14:22:44 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <Navbar/>
-      <Route exact path={'/'} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
+      <Route exact path={`/`} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
     </>
   );
 }
