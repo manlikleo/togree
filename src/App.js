919a531acaeb641ce9a143ef2a3159603b24ad39
manlikleo <olaotan104@gmail.com>
2020-12-04 14:21:31 +0300

    New Update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Pages/Home';
 import {BrowserRouter as Router} from 'react-router-dom';
 
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Route exact path={'/'} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
     </>
   );
