4a36efb9560f53c29bf51be19df4143f6b3bed2a
manlikleo <olaotan104@gmail.com>
2020-12-04 14:57:18 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Pages/Home';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById('root'));
 
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Route exact path={`/`} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/Home' exact component={Home} />
+        </Switch>
+      </Router>
     </>
   );
 }
