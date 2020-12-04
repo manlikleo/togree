4ba23318285b3c9acb0a2957c461a3e81a234683
manlikleo <olaotan104@gmail.com>
2020-12-04 15:15:12 +0300

    new pull

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Router>
-        {/* <Navbar /> */}
+        <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
