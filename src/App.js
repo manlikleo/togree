f0ea196ef7f46c23c3fd6c553e101fcf521499bf
manlikleo <olaotan104@gmail.com>
2020-12-04 14:19:37 +0300

    page redesign

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/Pages/Home';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path={'/'} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
-        </Switch>
-      </Router>
+      <Route exact path={'/'} render={ (routerProps) => < Home routerProps={routerProps} setUpGame={this.setUpGame} />} />
     </>
   );
 }
