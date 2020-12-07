651ae09034a9e88faee9198ea6fd1212f0675e0b
manlikleo <olaotan104@gmail.com>
2020-12-07 16:57:26 +0300

    final news update

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -18,54 +18,56 @@ import './slider.css';
 
             <div className="slider_container">
 
-            <div className="slider_content">
-                <h3 className="slider_title">Latest News</h3>
-                <p className="slider_subtitle">Not thoughts all exercise blessing. Indulgence way everything 
-joy alteration boisterous the attachment. Party we years to order allow asked of.               </p>
-            </div>         
-                    <div className="slidercards_containers">
-                    <i className="fas fa-angle-left togglebtns"></i>
-                            <ul className="slidercards_items">
-                                <Slidercard
-                                  className='slidercard_pic'
-                                 title='COVID-19 Disrups Transportation in Lagos'
-                                 subtitle='Musical by me through he drawing savings an. No we stand
-                                 avoid decay heard mr. Common so wicket appear to sudden 
-                                 worthy on. Shade of offer ye whole stood hoped. '
-                                 path='/'
-                                />
-                                <Slidercard
-                                   className='slidercard_pic slider1'
-                                 title='COVID-19 Disrups Transportation in Lagos'
-                                 subtitle='Musical by me through he drawing savings an. No we stand
-                                 avoid decay heard mr. Common so wicket appear to sudden 
-                                 worthy on. Shade of offer ye whole stood hoped. '
-                                 path='/'
-                                />
-                                <Slidercard
-                                   className='slidercard_pic slider2'
-                                 title='Seeing rather her you not esteem men settle'
-                                 subtitle='Musical by me through he drawing savings an. No we stand
-                                 avoid decay heard mr. Common so wicket appear to sudden 
-                                 worthy on. Shade of offer ye whole stood hoped. '
-                                 path='/'
-                                />
+                <div className="slider_content">
+                    <h3 className="slider_title">Latest News</h3>
+                    <p className="slider_subtitle">Not thoughts all exercise blessing. Indulgence way everything 
+                        joy alteration boisterous the attachment. Party we years to order allow asked of.              
+                    </p>
+                </div> 
 
+                        <div className="slidercards_containers">
+                            <i className="fas fa-angle-left togglebtns"></i>
                                 
-                            </ul>
-                            <i className="fas fa-angle-right togglebtns"></i>
-                    </div>
-                    <div className="sliderbtn">
-                    <Button 
-                    className="btns"
-                    buttonStyle="btn--primary"
-                    buttonSize="btn--small"
-                    >ALL NEWS ARTICLES <i className="fas fa-angle-right"></i></Button>
-                    </div>
+                                    <Slidercard
+                                    className='slidercard_pic'
+                                    title='COVID-19 Disrups Transportation in Lagos'
+                                    subtitle='Musical by me through he drawing savings an. No we stand
+                                    avoid decay heard mr. Common so wicket appear to sudden 
+                                    worthy on. Shade of offer ye whole stood hoped. '
+                                    path='/'
+                                    />
+                                    <Slidercard
+                                    className='slidercard_pic slider1'
+                                    title='COVID-19 Disrups Transportation in Lagos'
+                                    subtitle='Musical by me through he drawing savings an. No we stand
+                                    avoid decay heard mr. Common so wicket appear to sudden 
+                                    worthy on. Shade of offer ye whole stood hoped. '
+                                    path='/'
+                                    />
+                                    <Slidercard
+                                    className='slidercard_pic slider2'
+                                    title='Seeing rather her you not esteem men settle'
+                                    subtitle='Musical by me through he drawing savings an. No we stand
+                                    avoid decay heard mr. Common so wicket appear to sudden 
+                                    worthy on. Shade of offer ye whole stood hoped. '
+                                    path='/'
+                                    />
+                                
+
+                                <i className="fas fa-angle-right togglebtns"></i>
+                        </div>
+
+                        <div className="sliderbtn">
+                            <Button 
+                            className="btns"
+                            buttonStyle="btn--primary"
+                            buttonSize="btn--small"
+                            >ALL NEWS ARTICLES <i className="fas fa-angle-right"></i></Button>
+                        </div>
                   
         
                 
-            </div>
+                </div>
         )
     }
 }
--- src/components/Slider/Slidercard.js
+++ src/components/Slider/Slidercard.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom'
 function Slidercard(props) {
     return (
         <div className="slidercard_container">
-            <l1 className="slidercard_items">
+            <l1 className="sslidercards_items">
                 <div className={props.className}></div>
                 <div className="slidercard_content">
-                <h5 className="slidercard_title">{props.title}</h5>
-                <p className="slidercard_subtitle">{props.subtitle}</p>
-                <Link className="slidercard_link" to={props.path}>Read More<i className="fas fa-angle-right"></i> </Link>
+                    <h5 className="slidercard_title">{props.title}</h5>
+                    <p className="slidercard_subtitle">{props.subtitle}</p>
+                    <Link className="slidercard_link" to={props.path}>Read More<i className="fas fa-angle-right"></i> </Link>
                 </div>
             </l1>
         </div>
