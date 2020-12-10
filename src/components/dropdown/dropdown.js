7ff81007ec55b44a5adeb5d71a10529419d50e8d
manlikleo <olaotan104@gmail.com>
2020-12-10 16:51:05 +0300

    new update

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -7,43 +7,10 @@ import './dropdown.css'
     constructor(props){
     super(props)
         
-    this.state = {
+    this.state = {   
         ShowMenu:false,
         Setlanguage:""
     }    
-    }
-    render() {
-            const {ShowMenu,Setlanguage}= this.state;
-
-        return (
-            <div className={ShowMenu ? "dropdown active":"dropdown"}
-            onClick={this.handleClick}
-            >
-                <div className="dropdown_content">
-                    <div className="dropdown_text">
-                        {!Setlanguage? 'Select': Setlanguage}
-                    </div>
-
-                    <div className="drop_icon" onClick={this.Setlanguage}>
-                        <i className={this.state.Setlanguage ? 'fas fa-angle-down':'fas fa-angle-up'}> </i>
-                    </div>
-
-                </div>
-                    
-                        {Language.map((item,index)=>{
-
-                            return(
-                                <li>
-                                    <a className={item.cName}>
-                                        {item.title}
-                                    </a>
-                                </li>
-                            )
-                        })} 
-
-            
-            </div>
-        )
     }
 
 
@@ -53,25 +20,41 @@ import './dropdown.css'
 
     handleText = (ev) => {
         this.setState ({
-            HaveText:ev.currenttarget.textContent
+            haveText:ev.currentTarget.textContent
         })
     }
-
-    itemList = props => {
-        const list = props.map((item) => (
-            <div 
-            onClick={this.handleText}
-                className="dropdown__item"
-                key ={item.toString()}>
-                {item}
-            </div>
-        ));
+    
+    render() {
+            const {ShowMenu,Setlanguage}= this.state;
 
         return (
-            <div className="dropdown__items"> {list} </div>
-          )
+            <div className={ShowMenu ? "dropdown active":"dropdown"} onClick={this.handleClick}>
 
+                <div className="dropdown_content">
+                    <div className="dropdown_text">
+                        {!Setlanguage? 'EN': Setlanguage}
+                    </div>
 
+                    <div className="drop_icon" onClick={this.Setlanguage}>
+                        <i className={this.state.ShowMenu ? 'fas fa-angle-down':'fas fa-angle-up'}> </i>
+                    </div>
+
+                </div>
+                    
+                    <div className="dropdown_items" >
+                        {Language.map((item,index)=>{
+
+                            return(
+                                <li > <a className={item.cName}  onClick={this.handleText}>{item.title}</a></li> )})} 
+                    </div>
+            
+            </div>
+        )
     }
+
+
+    
+
+    
 }
 export default Dropdown
